72d782e1da152232d23f137f361cf3436d150cfb
AmmarAlam <ammaralam777@gmail.com>
2019-06-26 16:55:12 +0500

    edit

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,10 @@
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('/logout', 'Auth\LoginController@logout');
 
 Auth::routes();
